6dcd6dd7e560474227a77776029ed7fdb1fec92f
DaniloIS <dailosantos-pg@hotmail.com>
2022-02-22 14:06:14 -0300

    Organizando conteúdo com includes

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,52 @@ router.get('/', function(req, res, next) {
 
     }
 
-    res.render('index', { title: 'Restaurante Saboroso!', menu: results});
+    res.render('index', { title: 'Restaurante Saboroso!', menus: results});
 
+  });
+
+});
+
+/* GET contacts page. */
+router.get('/contacts', function(req, res, next) {
+
+  res.render('contacts', { 
+    title: 'Contato - Restaurante Saboroso!', 
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
+  });
+
+});
+
+/* GET menus page. */
+router.get('/menus', function(req, res, next) {
+
+  res.render('menus', { 
+    title: 'Menus - Restaurante Saboroso!', 
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
+  });
+
+});
+
+/* GET reservations page. */
+router.get('/reservations', function(req, res, next) {
+
+  res.render('reservations', { 
+    title: 'Reservas - Restaurante Saboroso!', 
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!!'
+  });
+
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+
+  res.render('services', { 
+    title: 'Serviços - Restaurante Saboroso!', 
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!!'
   });
 
 });
