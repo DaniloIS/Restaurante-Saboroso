2e52a1cc0356bc873316f4c4b95f5774a8b9182a
DaniloIS <dailosantos-pg@hotmail.com>
2022-02-22 16:27:20 -0300

    Refatorando Header, Footer e Reserva com POST para MySQL

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 var conn = require('./../inc/db');
+var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query("SELECT * FROM tb_menus ORDER BY title", (err, results) => {
+  menus.getMenus().then(results => {
 
-    if(err) {
-
-      console.log(err);
-
-    }
-
-    res.render('index', { title: 'Restaurante Saboroso!', menus: results});
+    res.render('index', { 
+      title: 'Restaurante Saboroso!', 
+      menus: results,
+      isHome: true
+    });
 
   });
 
@@ -33,10 +33,15 @@ router.get('/contacts', function(req, res, next) {
 /* GET menus page. */
 router.get('/menus', function(req, res, next) {
 
-  res.render('menus', { 
-    title: 'Menus - Restaurante Saboroso!', 
-    background: 'images/img_bg_1.jpg',
-    h1: 'Saboreie nosso menu!'
+  menus.getMenus().then(results => {
+
+    res.render('menus', { 
+      title: 'Menus - Restaurante Saboroso!', 
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu!', 
+      menus: results
+    });
+
   });
 
 });
@@ -44,11 +49,38 @@ router.get('/menus', function(req, res, next) {
 /* GET reservations page. */
 router.get('/reservations', function(req, res, next) {
 
-  res.render('reservations', { 
-    title: 'Reservas - Restaurante Saboroso!', 
-    background: 'images/img_bg_2.jpg',
-    h1: 'Reserve uma Mesa!!'
-  });
+  reservations.render(req, res);
+
+});
+
+/* GET reservations page. */
+router.post('/reservations', function(req, res, next) {
+
+  if(!req.body.name) {
+    reservations.render(req, res, "Digite o nome");
+  } else if(!req.body.email) {
+    reservations.render(req, res, "Digite o e-mail");
+  } else if(!req.body.people) {
+    reservations.render(req, res, "Selecione um número de pessoas");
+  } else if(!req.body.date) {
+    reservations.render(req, res, "Selecione a data");
+  } else if(!req.body.time) {
+    reservations.render(req, res, "Selecione a hora");
+  } else {
+
+    reservations.save(req.body).then(results => {
+
+      req.body = {};
+
+      reservations.render(req, res, null, "Reserva realizada com sucesso!");
+
+    }).catch(err => {
+
+      reservations.render(req, res, err.message);
+
+    });
+
+  }
 
 });
 
